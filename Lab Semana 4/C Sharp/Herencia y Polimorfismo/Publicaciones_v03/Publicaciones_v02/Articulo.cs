8b2df350fdc6e088cabef97fdfd2827cae07767a
R4MIRO13 <ramiro.gamboam@pucp.edu.pe>
2025-09-09 22:15:16 -0500

    Lab Semana 4

---
+++ Lab Semana 4/C Sharp/Herencia y Polimorfismo/Publicaciones_v03/Publicaciones_v02/Articulo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publicaciones_v03
+{
+    public class Articulo : Publicacion
+    {
+        private string revista;
+        private int volumen;
+        private int numero;
+        private string mes;
+
+        public Articulo(string nombre, string autor,int año, string revista, int volumen, int numero, string mes) : base(nombre, autor, año)
+        {
+            this.Revista = revista;
+            this.Volumen = volumen;
+            this.Numero = numero;
+            this.Mes = mes;
+        }
+
+        public string Revista { get => revista; set => revista = value; }
+        public int Volumen { get => volumen; set => volumen = value; }
+        public int Numero { get => numero; set => numero = value; }
+        public string Mes { get => mes; set => mes = value; }
+
+        public Articulo(string nombre, string[] autores, int año, string revista, int volumen, int numero, string mes) : base(nombre, autores, año)
+        {
+            this.Revista = revista;
+            this.Volumen = volumen;
+            this.Numero = numero;
+            this.Mes = mes;
+        }
+
+        public override string ToString()
+        {
+            string referencia = "[" + Publicacion.orden_publicacion + "] ";
+            for (int i = 0; i < this.Cantidad_autores; i++)
+            {
+                if (i != 0)
+                {
+                    if (i == this.Cantidad_autores - 1)
+                        referencia += " and ";
+                    else
+                        referencia += ", ";
+                }
+                referencia += this.Autores[i];
+            }
+            referencia += ". ";
+            referencia += this.Nombre;
+            referencia += ". ";
+            referencia += this.Volumen;
+            referencia += ". ";
+            referencia += this.Numero;
+            referencia += ". ";
+            referencia += this.Mes;
+            referencia += ". ";
+            referencia += this.Año;
+            referencia += ".";
+            return referencia;
+        }
+
+        public override string MetodoAbstracto()
+        {
+            return "Soy un articulo";
+        }
+    }
+}
---
+++ Lab Semana 4/C Sharp/Herencia y Polimorfismo/Publicaciones_v03/Publicaciones_v02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publicaciones_v03
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Publicacion[] publicaciones = new Publicacion[3];
+            //Publicacion articulo = new Articulo("A Relational Model of Data for Large Shared Data Banks", "Codd", 1970, "Communication of the ACM", 13, 6, "June");
+            publicaciones[0] = new Articulo("A Relational Model of Data for Large Shared Data Banks", "Codd", 1970, "Communication of the ACM", 13, 6, "June");
+            string[] autores = new string[] { "Gamma", "Helm", "Johnson", "Vlissides" };
+            //Publicacion libro = new Libro("Design Patterns: Elements of Reusable Object-Oriented Software", autores, 1994, "1st", "USA", "Addison-Wesley Professional");
+            publicaciones[1] = new Libro("Design Patterns: Elements of Reusable Object-Oriented Software", autores, 1994, "1st", "USA", "Addison-Wesley Professional");
+            //Publicacion tesis = new Tesis("A system of logic based on ordinals", "Alan Turing", 1938, "Tesis de doctorado", "Departamento de Matemáticas", "Universidad de Princeton", "Princeton", "NJ");
+            publicaciones[2] = new Tesis("A system of logic based on ordinals", "Alan Turing", 1938, "Tesis de doctorado", "Departamento de Matemáticas", "Universidad de Princeton", "Princeton", "NJ");
+
+            //articulo.Referenciar();
+            //libro.Referenciar();
+            //tesis.Referenciar();
+
+            //La linea siguiente no se puede implementar porque la clase es abstracta
+            //Publicacion pub = new Publicacion(publicacion[0]);
+
+            foreach(Publicacion publicacion in publicaciones)
+            {
+                publicacion.Referenciar();
+                //Console.WriteLine(publicacion.MetodoAbstracto());
+                Publicacion.orden_publicacion++;
+            }
+        }
+    }
+}
