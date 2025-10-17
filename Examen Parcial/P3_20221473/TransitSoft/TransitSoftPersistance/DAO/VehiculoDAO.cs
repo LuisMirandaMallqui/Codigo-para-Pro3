178ea575fc4f30f485693aa9314b86c81565dec2
R4MIRO13 <ramiro.gamboam@pucp.edu.pe>
2025-10-17 11:37:59 -0500

    Examen

---
+++ Examen Parcial/P3_20221473/TransitSoft/TransitSoftPersistance/DAO/VehiculoDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TransitSoftDomain;
+
+namespace TransitSoftPersistance.DAO
+{
+    public interface VehiculoDAO : BaseDAO<Vehiculo>
+    {
+        List<Vehiculo> ListarPorMarcaModelo(string marca_modelo);
+        List<string> ObtenerMarcas();
+    }
+}
---
+++ Examen Parcial/proyectostrategy/src/main/java/pe/edu/pucp/softinv/proyectostrategy/modelo/Gravedad.java
@@ -0,0 +1,7 @@
+package pe.edu.pucp.softinv.proyectostrategy.modelo;
+
+public enum Gravedad {
+    LEVE,
+    GRAVE,
+    MUY_GRAVE
+}
